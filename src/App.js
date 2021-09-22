c944338fe753b97d2c041756c734d5a0018abb63
hoapham96 <89959754+hoapham96@users.noreply.github.com>
2021-09-21 23:22:17 -0500

    added 2 clicks button to convert from c to f or from f to c

--- src/App.js
+++ src/App.js
@@ -5,31 +5,46 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {input:""};
+    this.state = {
+      input : "",
+      result: ""
+    };
+    // this.handleClick=this.handleClick.bind(this);
   }
 
   myChangeHandler = (event) => {
-    event.preventDefault()
-    this.setState({ input: event.target.value * (9 / 5) + 32 });
+    // event.preventDefault()
+    this.setState({ input: event.target.value });
   };
 
-  handleSubmit = (e) =>{ 
-    e.preventDefault();
-    this.setState({username: this.state.input})
+  convertFromCtoF= (e)=>{
+    e.preventDefault()
+    
+    this.setState({ result: this.state.input * (9 / 5) + 32 })
+
+  }
+
+  convertFromFtoC=(e) => {
+    e.preventDefault()
+
+    const value = this.state.input - 32 * 5 / 9
+    this.setState({result: value })
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit} >
         <h1>Temperature Converter </h1>
-        <p>Enter the temperator:</p>
+        <p>Enter the temperature:</p>
         <input
           type="text"
           onChange={this.myChangeHandler}
         />
-        <button>submit</button>
-        <p>Celsius to Fahrenheit</p>
-        <p>{this.state.username}</p>
+
+        <button onClick={this.convertFromCtoF}>Convert from C to F</button>
+        <button onClick={this.convertFromFtoC}>Convert from F to C</button>
+        <p>Celsius to Fahrenheit</p> 
+        <p>{this.state.result}</p>
       </form>
     );
   }
